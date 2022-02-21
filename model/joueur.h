57a3616d7f2e3733f9ba2c04a2e2f7a1a8cd0151
Rafael.Rojas-Galvan <rafael.rojas-galvan@insa-rennes.fr>
2022-02-21 14:50:57 +0000

    Update joueur.h

--- model/joueur.h
+++ model/joueur.h
@@ -4,5 +4,13 @@
 
 #ifndef MODEL_JOUEUR_H
 #define MODEL_JOUEUR_H
-
 #endif //MODEL_JOUEUR_H
+#define length 10
+//actions
+
+typedef enum{haut,bas,droite,gauche} Direction;
+typedef  struct{Direction d;unsigned int i;unsigned int j}Sousmarin;
+
+//Functions
+void afficherSousmarin(Sousmarin m);
+int avancer(Sousmarin * p_m);
