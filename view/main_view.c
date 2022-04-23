1002cc184e98c0cc593d7570b523d3c365e833ba
Alexis.Rabiller <arabille@insa-rennes.fr>
2022-04-23 10:23:51 +0200

    installation de cmocka pour les tests unitaire ainsi que dans le Makefile

--- view/main_view.c
+++ view/main_view.c
@@ -4,29 +4,114 @@
 
 /**
  * \file main_view.c
- * \brief Fonction principale de la vue.
+ * \brief Fichier principal de la vue.
  *
- * Fonction principale pour la gestion de la vue du projet
+ * Fichier principal pour la gestion de la vue du projet
  */
 
 #include "main_view.h"
 
 int init_view(View_elements * app) {
+    int Resolution_x =1600;
+    int Resolution_y=900;
     int statut = EXIT_FAILURE;
+    if (IMG_Init(IMG_INIT_JPG)==0){
+        fprintf(stderr, "Erreur IMG_Init : %s", SDL_GetError());
+        return statut;
+    }
+    if (TTF_Init()!=0){
+        fprintf(stderr, "Erreur TTF_Init : %s", SDL_GetError());
+        return statut;
+    }
+    init_font(app);
+    app->VBattlefield=(malloc(sizeof(View_Battlefield)));
+
     if (0 != SDL_Init(SDL_INIT_VIDEO)) {
         fprintf(stderr, "Erreur SDL_Init : %s", SDL_GetError());
         return statut;
     }
-    if (0 != SDL_CreateWindowAndRenderer(600, 6000, SDL_WINDOW_SHOWN, &app->pwindow, &app->pRenderer)) {
+    if (0 != SDL_CreateWindowAndRenderer(Resolution_x, Resolution_y, SDL_WINDOW_SHOWN, &app->wwindow, &app->rRenderer)) {
         fprintf(stderr, "Erreur SDL_CreateWindowAndRenderer : %s", SDL_GetError());
         return statut;
+    }
+    app->mMenu= SDL_CreateTexture(app->rRenderer,SDL_PIXELFORMAT_RGBA8888,SDL_TEXTUREACCESS_TARGET,Resolution_x,Resolution_y);
+    if(NULL == app->mMenu) {
+        fprintf(stderr, "Erreur SDL_CreateTexture : %s", SDL_GetError());
+        return statut;
+    }
+    app->rRegles= SDL_CreateTexture(app->rRenderer,SDL_PIXELFORMAT_RGBA8888,SDL_TEXTUREACCESS_TARGET,Resolution_x,Resolution_y);
+    if(NULL == app->rRegles) {
+        fprintf(stderr, "Erreur SDL_CreateTexture : %s", SDL_GetError());
+        return statut;
+    }
+    app->cCredit= SDL_CreateTexture(app->rRenderer,SDL_PIXELFORMAT_RGBA8888,SDL_TEXTUREACCESS_TARGET,Resolution_x,Resolution_y);
+    if(NULL == app->cCredit) {
+        fprintf(stderr, "Erreur SDL_CreateTexture : %s", SDL_GetError());
+        return statut;
+    }
+    app->cCarte= SDL_CreateTexture(app->rRenderer,SDL_PIXELFORMAT_RGBA8888,SDL_TEXTUREACCESS_TARGET,Resolution_x,Resolution_y);
+    if(NULL == app->cCarte) {
+        fprintf(stderr, "Erreur SDL_CreateTexture : %s", SDL_GetError());
+        return statut;
+    }
+    app->VBattlefield->Battlefield_blank =SDL_CreateTexture(app->rRenderer,SDL_PIXELFORMAT_RGBA8888,SDL_TEXTUREACCESS_TARGET,Resolution_x,Resolution_y);
+    if(NULL == app->VBattlefield->Battlefield_blank) {
+        fprintf(stderr, "Erreur SDL_CreateTexture : %s", SDL_GetError());
+        return statut;
+    }
+    app->VBattlefield->Battlefield_current =SDL_CreateTexture(app->rRenderer,SDL_PIXELFORMAT_RGBA8888,SDL_TEXTUREACCESS_TARGET,Resolution_x,Resolution_y);
+    if(NULL == app->VBattlefield->Battlefield_current) {
+            fprintf(stderr, "Erreur SDL_CreateTexture : %s", SDL_GetError());
+            return statut;
+    }
+    app->VBattlefield->Sous_marin =SDL_CreateTexture(app->rRenderer,SDL_PIXELFORMAT_RGBA8888,SDL_TEXTUREACCESS_TARGET,Resolution_x,Resolution_y);
+    if(NULL == app->VBattlefield->Sous_marin) {
+                fprintf(stderr, "Erreur SDL_CreateTexture : %s", SDL_GetError());
+                return statut;
     }
     statut=EXIT_SUCCESS;
     return statut;
 
 }
 
-int free_view(View_elements *app){
-    SDL_DestroyRenderer(app->pRenderer);
-    SDL_DestroyWindow(app->pwindow);
+void free_view(View_elements *app){
+    SDL_DestroyRenderer(app->rRenderer);
+    SDL_DestroyWindow(app->wwindow);
+    SDL_DestroyTexture(app->mMenu);
+    SDL_DestroyTexture(app->rRegles);
+    SDL_DestroyTexture(app->cCredit);
+    SDL_DestroyTexture(app->cCarte);
+    SDL_DestroyTexture(app->VBattlefield->Sous_marin);
+    SDL_DestroyTexture(app->VBattlefield->Battlefield_current);
+    SDL_DestroyTexture(app->VBattlefield->Battlefield_blank);
+    IMG_Quit();
+    TTF_Quit();
+    while (Mix_Init(0)){
+        Mix_Quit();
+    }
 }
+
+
+
+int init_all_view(View_elements *app){
+    if(init_menu(app)!=EXIT_SUCCESS){
+        return EXIT_FAILURE;
+    }
+    if(init_choix_carte(app) !=EXIT_SUCCESS){
+        return EXIT_FAILURE;
+    }
+    if(init_rules(app) !=EXIT_SUCCESS){
+        return EXIT_FAILURE;
+    }
+    if(init_credit(app) !=EXIT_SUCCESS){
+        return EXIT_FAILURE;
+    }
+    init_rbutton(app);
+    return EXIT_SUCCESS;
+}
+
+void init_font(View_elements * app){
+    app->ttf=TTF_OpenFont("Ressources/PermanentMarker-Regular.ttf",200);
+}
+
+
---
+++ view/rules_and_credit.c
@@ -0,0 +1,82 @@
+//
+// Created by robin on 19/02/2022.
+//
+/**
+ * \file rules_and_credit.c
+ * \brief Fichier Règles et Crédits.
+ *
+ * Fichier qui gère la création et l'affichage des règles et des crédits
+ */
+
+#include "main_view.h"
+
+void init_rbutton(View_elements *app){
+    SDL_Rect rect = {1470,0,130,120} ;
+    app->Rbutton=rect;
+}
+
+//Fonctions pour les Règles
+int init_rules(View_elements *app){
+    SDL_Texture * tmp=NULL;
+    SDL_Surface * surface=NULL;
+    surface=IMG_Load("Ressources/Rules.jpg");
+    if (surface==NULL){
+        fprintf(stderr, "Erreur SDL_CreateSurface : %s", SDL_GetError());
+        SDL_FreeSurface(surface);
+        SDL_DestroyTexture(tmp);
+        return EXIT_FAILURE;
+    }
+    tmp=SDL_CreateTextureFromSurface(app->rRenderer,surface);
+    if (tmp==NULL){
+        fprintf(stderr, "Erreur SDL_CreateTexture: %s", SDL_GetError());
+        SDL_FreeSurface(surface);
+        SDL_DestroyTexture(tmp);
+        return EXIT_FAILURE;
+    }
+    SDL_SetRenderTarget(app->rRenderer, app->rRegles);
+    SDL_Rect rect ={0,0,1600,900};
+    SDL_RenderCopy(app->rRenderer, tmp, &rect, &rect);
+    SDL_DestroyTexture(tmp);
+    SDL_FreeSurface(surface);
+    SDL_SetRenderTarget(app->rRenderer, NULL);
+    return EXIT_SUCCESS;
+}
+
+void show_rules(View_elements *app){
+    SDL_RenderCopy(app->rRenderer,app->rRegles,NULL,NULL);
+    SDL_RenderPresent(app->rRenderer);
+    app->window=Rules;
+}
+
+//Fonctions pour les Crédits
+int init_credit(View_elements *app){
+    SDL_Texture * tmp=NULL;
+    SDL_Surface * surface=NULL;
+    surface=IMG_Load("Ressources/Credits.jpg");
+    if (surface==NULL){
+        fprintf(stderr, "Erreur SDL_CreateSurface : %s", SDL_GetError());
+        SDL_FreeSurface(surface);
+        SDL_DestroyTexture(tmp);
+        return EXIT_FAILURE;
+    }
+    tmp=SDL_CreateTextureFromSurface(app->rRenderer,surface);
+    if (tmp==NULL){
+        fprintf(stderr, "Erreur SDL_CreateTexture: %s", SDL_GetError());
+        SDL_FreeSurface(surface);
+        SDL_DestroyTexture(tmp);
+        return EXIT_FAILURE;
+    }
+    SDL_SetRenderTarget(app->rRenderer, app->cCredit);
+    SDL_Rect rect ={0,0,1600,900};
+    SDL_RenderCopy(app->rRenderer, tmp, &rect, &rect);
+    SDL_DestroyTexture(tmp);
+    SDL_FreeSurface(surface);
+    SDL_SetRenderTarget(app->rRenderer, NULL);
+    return EXIT_SUCCESS;
+}
+
+void show_credit(View_elements *app){
+    SDL_RenderCopy(app->rRenderer,app->cCredit,NULL,NULL);
+    SDL_RenderPresent(app->rRenderer);
+    app->window=Credits;
+}
