395a4bcf9e1334711dffe210141d6db08d6d7f8a
Audrey.Cornec <audrey.cornec@insa-rennes.fr>
2022-04-29 09:26:48 +0000

    suite test unitaires modele

--- model/UTest_modele.c
+++ model/UTest_modele.c
@@ -20,7 +20,7 @@ static int setup_joueur(void **state){
         return -1;
     }
     j->energie=0;
-    j->vie=0;
+    j->vie=2;
     j->S_M->ligne=5;
     j->S_M->colonne=5;
 
@@ -56,7 +56,7 @@ static void test_energie_down(void **state){
 static void test_deplacement_haut(void **state){
     JOUEUR *j =(JOUEUR *)* state;
     deplacement(, J1, haut);
-    assert_int_equal(j->S_M->ligne, 4); //pas 1 mais +1 de la ou ca a commence
+    assert_int_equal(j->S_M->ligne, 4);
     assert_int_equal(j->S_M->colonne, 5);
 
 }
@@ -64,24 +64,68 @@ static void test_deplacement_haut(void **state){
 static void test_deplacement_bas(void **state){
     JOUEUR *j =(JOUEUR *)* state;
     deplacement(, J1, bas);
-    assert_int_equal(j->S_M->ligne, 6); //pas 1 mais +1 de la ou ca a commence
+    assert_int_equal(j->S_M->ligne, 6);
     assert_int_equal(j->S_M->colonne, 5);
 }
 static void test_deplacement_droite(void **state){
     JOUEUR *j =(JOUEUR *)* state;
     deplacement(, J1, droite);
-    assert_int_equal(j->S_M->ligne, 5); //pas 1 mais +1 de la ou ca a commence
+    assert_int_equal(j->S_M->ligne, 5);
     assert_int_equal(j->S_M->colonne, 6);
 }
 static void test_deplacement_gauche(void **state){
     JOUEUR *j =(JOUEUR *)* state;
     deplacement(, J1, droite);
-    assert_int_equal(j->S_M->ligne, 5); //pas 1 mais +1 de la ou ca a commence
+    assert_int_equal(j->S_M->ligne, 5);
     assert_int_equal(j->S_M->colonne, 4);
 }
+//dplcmnt possible fin carte + rocher
 
+static void enough_energie_missile(void **state){
+    JOUEUR *j =(JOUEUR *)* state;
+    energie_up(j);
+    energie_up(j);
+    assert_int_equal(enough_energie(, J1, MIS), 1);
+}
 
+static void not_enough_energie_missile(void **state){
+    JOUEUR *j =(JOUEUR *)* state;
+    assert_int_equal(enough_energie(, J1, MIS), 0);
+}
 
+static void enough_energie_surface(void **state){
+    JOUEUR *j =(JOUEUR *)* state;
+    assert_int_equal(enough_energie(, J1, SURF), 1);
+}
+
+static void enough_energie_sonar(void **state){
+    JOUEUR *j =(JOUEUR *)* state;
+    energie_up(j);
+    assert_int_equal(enough_energie(, J1, SON), 1);
+}
+
+static void not_enough_energie_sonar(void **state){
+    JOUEUR *j =(JOUEUR *)* state;
+    energie_down(j);
+    assert_int_equal(enough_energie(, J1, SON), 0);
+}
+
+static void enough_energie_silence(void **state){
+    JOUEUR *j =(JOUEUR *)* state;
+    energie_up(j);
+    assert_int_equal(enough_energie(, J1, SIL), 1);
+}
+
+static void not_enough_energie_silence(void **state){
+    JOUEUR *j =(JOUEUR *)* state;
+    assert_int_equal(enough_energie(, J1, SIL), 0);
+}
+
+static void enough_energie_dplcmt(void **state){
+    JOUEUR *j =(JOUEUR *)* state;
+    energie_down(j,2);
+    assert_int_equal(enough_energie(, J1, DEPLCMNT), 1);
+}
 
 int main(void)
 {
@@ -89,7 +133,19 @@ int main(void)
     const struct CMUnitTest tests_joueur[]=
             {
                     cmocka_unit_test_setup_teardown(test_energie_up, setup_joueur, teardown),
+                    cmocka_unit_test_setup_teardown(test_energie_down, setup_joueur, teardown),
+                    cmocka_unit_test_setup_teardown(test_deplacement_haut, setup_joueur, teardown),
+                    cmocka_unit_test_setup_teardown(test_deplacement_bas, setup_joueur, teardown),
+                    cmocka_unit_test_setup_teardown(test_deplacement_gauche, setup_joueur, teardown),
+                    cmocka_unit_test_setup_teardown(test_deplacement_droite, setup_joueur, teardown),
+                    cmocka_unit_test_setup_teardown(enough_energie_missile, setup_joueur, teardown),
+                    cmocka_unit_test_setup_teardown(not_enough_energie_missile, setup_joueur, teardown),
+                    cmocka_unit_test_setup_teardown(enough_energie_surface, setup_joueur, teardown),
+                    cmocka_unit_test_setup_teardown(enough_energie_sonar, setup_joueur, teardown),
+                    cmocka_unit_test_setup_teardown(not_enough_energie_sonar, setup_joueur, teardown),
+                    cmocka_unit_test_setup_teardown(enough_energie_dplcmt, setup_joueur, teardown),
+
 
             };
-    return cmocka_run_group_tests_name("Test joueur module",tests_joueur,NULL,NULL);
+    return cmocka_run_group_tests_name("Test joueur module", tests_joueur, NULL, NULL);
 }
