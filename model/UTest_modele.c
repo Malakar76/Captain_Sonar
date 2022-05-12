6d91d10cc08b3b28922524796999cefb0da9a6f4
Aurey Cornec <audrey.cornec@insa-rennes.fr>
2022-05-12 14:30:11 +0200

    test unitaires suites de modele

--- model/UTest_modele.c
+++ model/UTest_modele.c
@@ -96,6 +96,25 @@ static void test_deplacement_droite(void **state){
     deplacement(pg, J1, haut);
     assert_int_equal(j->energie, 4); //test si energie deja a 4, ca bouge pas
 }
+
+static void test_deplacement_droite2(void **state){
+    Playground *pg= (Playground *)* state;
+
+    init_model(pg);
+
+    deplacement(pg, J1, droite);
+
+    assert_int_equal(pg->J1->S_M->ligne, 0);
+    // assert_int_equal(j->S_M->colonne, 1);
+    // assert_int_equal(j->nbpath,1);
+    assert_string_equal(pg->J1->path, haut);
+    assert_int_equal(pg->J1->calqueJ[pg->J1->S_M->ligne][pg->J1->S_M->colonne],1);
+
+    assert_int_equal(pg->J1->energie, 3);
+    energie_up(pg->J1);
+    deplacement(pg, J1, haut);
+    assert_int_equal(pg->J1->energie, 4); //test si energie deja a 4, ca bouge pas
+}
 /*static void test_deplacement_haut(void **state){
     JOUEUR *j =(JOUEUR *)* state;
     init_calque(j);
@@ -203,12 +222,89 @@ static void enough_energie_dplcmt(void **state){
     assert_int_equal(enough_energie(, J1, DEPLCMNT), 1);
 }
 
-static void test_missile(void **state){
-    JOUEUR *j =(JOUEUR *)* state;
 
-    assert_int_equal(enough_energie(, J1, DEPLCMNT), 1);
-}
+
 */
+static void test_init_joueur(void **state){
+    JOUEUR *j =(JOUEUR *)* state;
+    init_joueur(j);
+    assert_int_equal(j->energie, 0);
+    assert_int_equal(j->vie, 2);
+    assert_int_equal(j->S_M->ligne, 0);
+    assert_int_equal(j->S_M->colonne, 0);
+    assert_int_equal(j->S_M->start[0], 0);
+    assert_int_equal(j->S_M->start[1], 0);
+    assert_int_equal(j->nbpath, 0);
+}
+
+_Noreturn static void test_init_calque(void **state) {
+    int i,k;
+    JOUEUR *j = (JOUEUR *) *state;
+    init_calque(j);
+    for (i = 0; i < 10; i++) {
+        for (k = 0; k < 10; k++) {
+            assert_int_equal(j->calqueJ[i][k], 0);
+        }
+    }
+}
+static void test_init_calque_bis(void **state) { //test que 2 cases mais pb vec le fct originel on ya un _Noreturn
+    JOUEUR *j = (JOUEUR *) *state;
+    init_calque(j);
+    assert_int_equal(j->calqueJ[0][0], 0);
+    assert_int_equal(j->calqueJ[9][9], 0);
+}
+
+static void test_init_reset(void **state){
+    JOUEUR *j =(JOUEUR *)* state;
+    reset_joueur(j);
+
+    assert_int_equal(j->energie, 0);
+    assert_int_equal(j->vie, 2);
+    assert_int_equal(j->S_M->ligne, 0);
+    assert_int_equal(j->S_M->colonne, 0);
+    assert_int_equal(j->S_M->start[0], 0);
+    assert_int_equal(j->S_M->start[1], 0);
+    assert_int_equal(j->nbpath, 0);
+
+
+}
+
+static void test_start_Sous_Marin(void **state) {
+    JOUEUR *j = (JOUEUR *) *state;
+    CARTE *c= malloc(sizeof (CARTE));
+    start_Sous_Marin(j,5,7,c);
+
+    assert_int_equal(j->S_M->ligne, 5);
+    assert_int_equal(j->S_M->colonne, 7);
+    assert_int_equal(j->S_M->start[0],j->S_M->ligne);
+    assert_int_equal(j->S_M->start[1],j->S_M->colonne);
+    assert_int_equal(j->calqueJ[5][7],1);
+    assert_int_equal(c->carte[5][7].sous_marin,1);
+}
+
+static void test_missile(void **state){
+    JOUEUR *j1 =(JOUEUR *)* state;
+    pg->J1=j;
+    JOUEUR *j2 =(JOUEUR *)* state;
+    pg->J2=j2;
+    deplacement(pg, J2, droite);
+    deplacement(pg, J2, bas); //on a placé le j2 à la pos 1x1
+    missile(, J1, 1, 1); //j1 tire sur J2
+
+    assert_int_equal(j1->vie, 2); //j1 garde ses 2 vies
+    assert_int_equal(j2->vie, 1); //j2 a ete touché il a perdu 1 vie
+
+    missile(pg, J1, 0, 0); //j1 tire sur lui meme
+
+    assert_int_equal(j1->vie, 1); //j1 perd une vie
+
+    missile(pg, J1, 0, 0); //j1 tire sur rien
+
+    assert_int_equal(j1->vie, 1); //j1 n a pas ete touche
+    assert_int_equal(j2->vie, 1); //j2 n a pas ete touche
+}
+
+
 int main(void)
 {
 
@@ -216,9 +312,9 @@ int main(void)
             {
                     cmocka_unit_test_setup_teardown(test_energie_up, setup_joueur, teardown),
                     cmocka_unit_test_setup_teardown(test_energie_down, setup_joueur, teardown),
-                    cmocka_unit_test_setup_teardown(test_deplacement_droite, setup_joueur, teardown),
+                        /*cmocka_unit_test_setup_teardown(test_deplacement_droite, setup_joueur, teardown),
 
-                    /* cmocka_unit_test_setup_teardown(test_deplacement_haut, setup_joueur, teardown),
+                 cmocka_unit_test_setup_teardown(test_deplacement_haut, setup_joueur, teardown),
                      cmocka_unit_test_setup_teardown(test_deplacement_bas, setup_joueur, teardown),
                      cmocka_unit_test_setup_teardown(test_deplacement_gauche, setup_joueur, teardown),
                      cmocka_unit_test_setup_teardown(enough_energie_missile, setup_joueur, teardown),
@@ -226,7 +322,12 @@ int main(void)
                      cmocka_unit_test_setup_teardown(enough_energie_surface, setup_joueur, teardown),
                      cmocka_unit_test_setup_teardown(enough_energie_sonar, setup_joueur, teardown),
                      cmocka_unit_test_setup_teardown(not_enough_energie_sonar, setup_joueur, teardown),
-                     cmocka_unit_test_setup_teardown(enough_energie_dplcmt, setup_joueur, teardown),*/
+                     cmocka_unit_test_setup_teardown(enough_energie_dplcmt, setup_joueur, teardown),
+                     cmocka_unit_test_setup_teardown(test_missile, setup_joueur, teardown),*/
+                     cmocka_unit_test_setup_teardown(test_init_joueur, setup_joueur, teardown),
+                     cmocka_unit_test_setup_teardown(test_init_calque, setup_joueur, teardown),
+                    cmocka_unit_test_setup_teardown(test_init_reset, setup_joueur, teardown),
+                    cmocka_unit_test_setup_teardown(test_start_Sous_Marin, setup_joueur, teardown),
 
 
             };
